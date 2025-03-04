f57ced59063636c6de5d59f3fe3d26fd6b3ce8e3
Mike Jin <mikejin@outlook.com>
2025-03-04 18:58:45 -0500

    deploy

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom"
+import '../css/NavBar.css'
+
+function NavBar() {
+    return <nav className="navbar">
+
+        hello!
+ {/* 
+
+
+<div className="navbar-brand"> 
+            <Link to="/">Movie App</Link>
+        </div>
+        <div className="navbar-links"> 
+            <Link to="/" className="nav-link">Home</Link>
+            <Link to="/favorites" className="nav-link">Favotites</Link>
+        </div>
+  */}
+
+        
+    </nav>
+}
+
+export default NavBar  
---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,26 @@
+import '../css/Favorites.css'
+import { useMovieContext } from '../contexts/MovieContext'
+import MovieCard from '../components/MovieCard'
+
+function Favorites() {
+    const { favorites } = useMovieContext()
+    if (favorites) {
+        return (
+        <div className="favorites">
+            <h2>Your Favorites</h2>
+            <div className="movies-grid">
+                {favorites.map((movie) => (
+                    <MovieCard movie={movie} key={movie.id}/>
+                    )
+                )}
+            </div>
+        </div>)
+    }
+    //else
+    return <div className="favorite-empty">
+        <h2>No Favorite Movies Yet</h2>
+        <p></p>
+    </div>
+}
+
+export default Favorites
