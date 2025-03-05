b0b2811772e37d352023a8529383acb9a4bc5f07
Mike Jin <mikejin@outlook.com>
2025-03-04 19:07:37 -0500

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-//import './css/App.css'
-import './App.css'
+import './css/App.css'
 import MovieCard from "./components/MovieCard"
 import Home from "./pages/Home"
 import Favorites from './pages/Favorites'
