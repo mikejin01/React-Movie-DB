3a8b0509771d0be7c7f68701680f1c657790836f
Mike Jin <mikejin@outlook.com>
2025-03-04 19:16:48 -0500

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import './css/App.css'
 import MovieCard from "./components/MovieCard"
 import Home from "./pages/Home"
 import Favorites from './pages/Favorites'
-//import {Routes, Route} from "react-router-dom"
+import {Routes, Route} from "react-router-dom"
 import NavBar from './components/NavBar'
 import { MovieProvider } from './contexts/MovieContext'
 
@@ -17,11 +17,11 @@ function App() {
       <main className='main-content'>
 
         
-      {/*}
+
         <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="/favorites" element={<Favorites />}/>
-        </Routes>*/}
+        </Routes>
   
         
 
